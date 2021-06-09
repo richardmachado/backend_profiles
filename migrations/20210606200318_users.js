c34053ea8a80e45756dec1150c36f51fc2a68baa
Richard Machado <mmachado.richard@gmail.com>
2021-06-09 12:15:06 -0700

    replaces double quotes with single quotes

--- migrations/20210606200318_users.js
+++ migrations/20210606200318_users.js
@@ -1,12 +1,12 @@
 exports.up = async function (knex) {
-  await knex.schema.createTable("users", (table) => {
-    table.increments("id").unique();
-    table.string("username").unique().notNullable();
-    table.string("password").notNullable();
+  await knex.schema.createTable('users', (table) => {
+    table.increments('id').unique();
+    table.string('username').unique().notNullable();
+    table.string('password').notNullable();
     table.timestamps(true, true);
   });
 };
 
 exports.down = async function (knex) {
-  await knex.schema.dropTableIfExists("users");
+  await knex.schema.dropTableIfExists('users');
 };
