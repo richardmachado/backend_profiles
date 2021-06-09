a8cc3535fcd851350207b36d1cbeeebeeab99cab
Richard Machado <mmachado.richard@gmail.com>
2021-06-09 13:18:47 -0700

    adds dotenv to files that require it

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,7 +1,9 @@
 const knex = require("knex");
 const knexfile = require("../knexfile.js");
+require("dotenv").config();
 
-const env = process.env.NODE_ENV || "production";
+const env = process.env.NODE_ENV || "development";
+
 const config = knexfile[env];
 console.log("you are currently working on", env, "server");
 
--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,4 @@
-// Update with your config settings.
-
-DATABASE_URL = "postgres://hwxsazcyjtxwpz:7e0f22a4180fb79a4041e304dfd2917cf7472f02e219e9efa3618f01370e4147@ec2-34-230-115-172.compute-1.amazonaws.com:5432/d5t35roe4ctghn"
+require("dotenv").config();
 
 module.exports = {
   development: {
@@ -29,15 +27,11 @@ module.exports = {
 
   production: {
     client: "pg",
-    connection: `${DATABASE_URL}`,
-    pool: {
-      min: 2,
-      max: 10,
-    },
+    connection: `${process.env.DATABASE_URL}?ssl=no-verify`,
     migrations: {
-      tableName: "./migrations",
+      directory: "./migrations",
     },
     seeds: { directory: "./seeds" },
   },
 };
-console.log("database", DATABASE_URL)
+
