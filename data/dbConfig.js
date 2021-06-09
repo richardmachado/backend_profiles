7aa4a5e7e399fb2c506bf957d9caa430c47c3f91
Richard Machado <mmachado.richard@gmail.com>
2021-06-09 12:10:01 -0700

    updated seeds

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -3,6 +3,6 @@ const knexfile = require("../knexfile.js");
 
 const env = process.env.NODE_ENV || "development";
 const config = knexfile[env];
-console.log("you are currently working on ", env, "server");
+console.log("you are currently working on", env, "server");
 
 module.exports = knex(config);
